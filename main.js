5ec651bbb6211d3b66b2c037b12690dae9b4f401
Phil Corbett <corbett.phil@gmail.com>
2022-01-25 09:17:24 -0500

    trying a different way of showing linkage

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ var api = (function() {
     // Elements
     const output = document.getElementById('output');
     const txtNewTitle = document.getElementById('newTitle');
+    const txtNewLabels = document.getElementById('newLabels');
     const btnAddNode = document.getElementById('newAdd');
     const btnReset = document.getElementById('reset');
     const txtData = document.getElementById('data');
 
     // Events
     btnAddNode.addEventListener('click', function() {
-        add(txtNewTitle.value);
+        var id = add(txtNewTitle.value, txtNewLabels.value);
+        print();
     });
     btnReset.addEventListener('click', function() {
         data = JSON.parse(txtData.value);
@@ -19,7 +21,8 @@ var api = (function() {
     var data = {
         currentNode: '',
         nodes:  {},
-        links: {}
+        links: {},
+        labels: []
     };
     
     function uuidv4() {
@@ -34,22 +37,74 @@ var api = (function() {
     }
 
     function init() {
-        add('Beginning');
-        add('Middle A');
-        add('Middle B');
-        add('End');
+        add('Beginning', '+alive');
+        add('Travel to the Inn', '+thief,+key,#boss,*alive');
+        add('Travel to the Cave', '+paladin,#boss,*alive');
+        add('Travel to the Church', '-paladin,#boss,*alive');
+        add('Fight Boss', '*key,-paladin,+boss,#boss,*alive,#avoid');
+        add('Win Boss', '*boss,+wand,+child,*alive,-boss,#avoid,+win,#win')
+        add('Lose to Boss', '*boss,-boss,-paladin,-thief,-wizard,-alive,*alive,#avoid')
+        add('Avoid Boss', '*thief,+wand,+child,#boss,*alive,+avoid,+win,#win')
+        add('Find Wizard', '*wand,*alive')
+
+        // add('Beginning','');
+        // add('Travel to the Inn', '');
+        // add('Travel to the Cave', '');
+        // add('Travel to the Church', '');
+        // add('Fight Boss', '');
+        // add('Win Boss', '')
+        // add('Lose to Boss', '')
+        // add('Avoid Boss', '')
+        // add('Find Wizard', '')
+
         print();
     }
 
-    function add(title) {
+    function add(title, labels) {
         var id = uuidv4();
         data.nodes[id] = {
             id: id,
             title: title,
-            description: ''
+            description: '',
+            done: false,
+            labels: {
+                add: [],
+                remove: [],
+                require: [],
+                avoid: []
+            }
         };
         createLink(id, id, 'self');
-        print();
+        parseLabels(id, labels);
+        return id;
+    }
+
+    function parseLabels(id, labels) {
+       var labelStructure = {
+            add: [],
+            remove: [],
+            require: [],
+            avoid: []
+        };
+        var labelArray = labels.split(',');
+        
+        labelArray.forEach(l => {
+            if(l[0] == '+') {
+                labelStructure.add.push(l.substring(1))
+            }
+            if(l[0] == '-') {
+                labelStructure.remove.push(l.substring(1))
+            }
+
+            if(l[0] == '*') {
+                labelStructure.require.push(l.substring(1))
+            }
+            if(l[0] == '#') {
+                labelStructure.avoid.push(l.substring(1))
+            }
+        });
+
+        data.nodes[id].labels = labelStructure;
     }
 
     function setCurrentNode(id) {
@@ -58,12 +113,16 @@ var api = (function() {
     }
 
     function createLink(prevId, nextId, relationship) {
-        data.links[linkId(prevId, nextId)] = {
-            prevId: prevId,
-            nextId: nextId,
-            relationship: relationship,
-            description: ''
-        };
+        // data.links[linkId(prevId, nextId)] = {
+        //     prevId: prevId,
+        //     nextId: nextId,
+        //     relationship: relationship,
+        //     description: ''
+        // };
+
+        if(relationship=='yes') {
+
+        }
         print();
     }
 
@@ -72,21 +131,78 @@ var api = (function() {
         print();
     }
 
-    function nodeToHtml(id, history) {
+    function nodeToHtml(id, history, labels) {
         var rtnHtml = '';
         var cHistory = JSON.parse(JSON.stringify(history));
-        cHistory[id] = 'done';
-        rtnHtml = '<li>' + data.nodes[id].title + '<ul>';
+        var cLabels = JSON.parse(JSON.stringify(labels));
+        var node = data.nodes[id];
 
-        Object.keys(data.links).forEach(linkId => {
-            var l = data.links[linkId];
-            if(l.prevId == id && l.relationship == 'yes' && !cHistory[l.nextId]) {
-                rtnHtml += nodeToHtml(l.nextId, cHistory);
+        cHistory[id] = 'done';
+        rtnHtml = '<li>' + node.title + '<ul>';
+
+        node.labels.add.forEach(a => {
+            cLabels[a] = true;
+        });
+
+        node.labels.remove.forEach(r => {
+            cLabels[r] = false;
+        });
+
+        // Object.keys(data.links).forEach(linkId => {
+        //     var l = data.links[linkId];
+
+        //     // Do I want to get rid of the direct relationships and just say after?
+        //     // Do I want to use labels as the requirements for building out the tree?
+        //     if(l.prevId == id
+        //         && l.relationship == 'yes'
+        //         && !cHistory[l.nextId]
+        //         && meetsLabelRequirements(l.nextId, cLabels)
+        //         && isViableFutureNode(l.nextId)) {
+        //         rtnHtml += nodeToHtml(l.nextId, cHistory, cLabels);
+        //     }
+        // });
+
+        Object.keys(data.nodes).forEach(nodeId => {
+            var n = data.nodes[nodeId];
+
+            // Do I want to get rid of the direct relationships and just say after?
+            // Do I want to use labels as the requirements for building out the tree?
+            if(!cHistory[nodeId]
+                && meetsLabelRequirements(nodeId, cLabels)
+                && isViableFutureNode(nodeId)) {
+                rtnHtml += nodeToHtml(nodeId, cHistory, cLabels);
             }
         });
 
         rtnHtml += '</ul></li>'; 
         return rtnHtml;
+    }
+
+    function meetsLabelRequirements(id, labels) {
+        var node = data.nodes[id];
+        var meets = true;
+
+        node.labels.require.forEach(l => {
+            if (!labels[l]) {
+                meets = false;
+            }
+        })
+
+        if(meets) {
+            node.labels.avoid.forEach(l => {
+                if (labels[l]) {
+                    meets = false;
+                }
+            }) 
+        }
+
+        return meets;
+    }
+
+    function isViableFutureNode(id) {
+        var n = data.nodes[id];
+
+        return !n.done;
     }
 
     function print() {
@@ -98,7 +214,7 @@ var api = (function() {
         if(data.currentNode != '') {
             current = '<div style="border: 1px solid #ccc;">Current: ' + data.nodes[data.currentNode].title + '<ul>';
 
-            current += nodeToHtml(data.currentNode, {});
+            current += nodeToHtml(data.currentNode, {}, {});
 
             current += '</ul></div>';
         }
